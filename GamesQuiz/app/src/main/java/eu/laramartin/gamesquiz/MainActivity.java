1f782b727caa5c1192b994422d10d3f45cfc4912
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-21 13:30:21 +0200

    checking question 1. changed quest 5

--- GamesQuiz/app/src/main/java/eu/laramartin/gamesquiz/MainActivity.java
+++ GamesQuiz/app/src/main/java/eu/laramartin/gamesquiz/MainActivity.java
@@ -2,12 +2,49 @@ package eu.laramartin.gamesquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(submitButtonOnClikListener);
     }
+
+    int correctAnswers = 0;
+
+
+    // final scores displayed in a toast
+
+    private void checkQuestionOneAnswers(){
+        CheckBox questionOneMario = (CheckBox) findViewById(R.id.checkboxQuest1AnswerMario);
+        CheckBox questionOneDonkey = (CheckBox) findViewById(R.id.checkboxQuest1AnswerDonkey);
+        CheckBox questionOnePortal = (CheckBox) findViewById(R.id.checkboxQuest1AnswerPortal);
+        boolean isQuestionOneMarioChecked = questionOneMario.isChecked();
+        boolean isQuestionOneDonkeyChecked = questionOneDonkey.isChecked();
+        boolean isQuestionOnePortalChecked = questionOnePortal.isChecked();
+
+        if (isQuestionOneMarioChecked && isQuestionOneDonkeyChecked && !isQuestionOnePortalChecked){
+            correctAnswers += 1;
+        }
+    }
+
+    final View.OnClickListener submitButtonOnClikListener = new View.OnClickListener() {
+        public void onClick(final View v){
+            checkQuestionOneAnswers();
+            Toast.makeText(MainActivity.this, "Correct Answers: " + correctAnswers + "/5", Toast.LENGTH_LONG).show();
+        }
+    };
+
+
+
 }
